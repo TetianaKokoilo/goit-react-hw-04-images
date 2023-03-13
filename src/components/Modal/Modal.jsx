ccfb3177fef68348f8af16ba262729b3640a24a7
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-03-13 19:37:13 +0200

    add hooks for searchbar

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,44 +1,61 @@
-import { Component } from 'react';
+/* eslint-disable no-unused-vars */
+import { useEffect } from 'react';
 import { createPortal } from 'react-dom';
 import { StyledOverlay, StyledModal } from './Modal.styled';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 const modalRoot = document.querySelector('#modal-root');
 
-export class Modal extends Component {
-  componentDidMount() {
-    // console.log('Modal componentDidMount');
-    window.addEventListener('keydown', this.hadleKeyDown);
-  }
-
-  componentWillUnmount() {
-    // console.log('Modal componentWillUnmount');
-    window.removeEventListener('keydown', this.hadleKeyDown);
-  }
-
-  hadleKeyDown = e => {
-    if (e.code === 'Escape') {
-      this.props.onClose();
+export const Modal = ({ onClose, children }) => {
+  useEffect(() => {
+    const hadleKeyDown = e => {
+      if (e.code === 'Escape') {
+        onClose();
+      }
     }
-  };
+  }, [onClose])
 
-  handleBackdropClick = e => {
-    if (e.currentTarget === e.target) {
-      this.props.onClose();
-    }
-  };
+  return createPortal(
+    <StyledOverlay onClick={this.handleBackdropClick}>
+      <StyledModal>{children}</StyledModal>
+    </StyledOverlay>,
+    modalRoot
+  );
+};
+// export class Modal extends Component {
+//   componentDidMount() {
+//     // console.log('Modal componentDidMount');
+//     window.addEventListener('keydown', this.hadleKeyDown);
+//   }
 
-  render() {
-    const children = this.props.children;
-    return createPortal(
-      <StyledOverlay onClick={this.handleBackdropClick}>
-            <StyledModal>{children}</StyledModal>
-      </StyledOverlay>,
-      modalRoot
-    );
-  }
-}
+//   componentWillUnmount() {
+//     // console.log('Modal componentWillUnmount');
+//     window.removeEventListener('keydown', this.hadleKeyDown);
+//   }
 
-Modal.propTypes = {
-  children: PropTypes.element.isRequired,
-}
+//   hadleKeyDown = e => {
+//     if (e.code === 'Escape') {
+//       this.props.onClose();
+//     }
+//   };
+
+//   handleBackdropClick = e => {
+//     if (e.currentTarget === e.target) {
+//       this.props.onClose();
+//     }
+//   };
+
+//   render() {
+//     const children = this.props.children;
+//     return createPortal(
+//       <StyledOverlay onClick={this.handleBackdropClick}>
+//             <StyledModal>{children}</StyledModal>
+//       </StyledOverlay>,
+//       modalRoot
+//     );
+//   }
+// }
+
+// Modal.propTypes = {
+//   children: PropTypes.element.isRequired,
+// }
--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -5,48 +5,44 @@ import {
   StyledSearchFormButtonLabel,
   StyledSearchFormInput,
 } from './Searchbar.styled';
-import { Component } from 'react';
+import { useState } from 'react';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+export const Searchbar = ({ onSubmit }) => {
+  const [searchImage, setSearchImage] = useState('');
 
-export class Searchbar extends Component {
-  state = {
-    searchImage: '',
-  };
-
-  handleChange = e => {
+  const handleChange = e => {
     const value = e.target.value.toLowerCase();
-    this.setState({ searchImage: value });
+    setSearchImage(value);
   };
 
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.searchImage.trim() === '') {
-      return Notify.info('Please enter the name of the picture for the request');
+    if (searchImage.trim() === '') {
+      return Notify.info(
+        'Please enter the name of the picture for the request'
+      );
     }
-    this.props.onSubmit(this.state.searchImage);
-    this.setState({ searchImage: '' });
+    onSubmit(searchImage);
+    setSearchImage('');
   };
 
-  render() {
-    const { searchImage } = this.state;
-    return (
-      <StyledSearchbar onSubmit={this.handleSubmit}>
-        <StyledSearchForm>
-          <StyledSearchFormButton type="submit">
-            <StyledSearchFormButtonLabel>Search</StyledSearchFormButtonLabel>
-          </StyledSearchFormButton>
+  return (
+    <StyledSearchbar onSubmit={handleSubmit}>
+      <StyledSearchForm>
+        <StyledSearchFormButton type="submit">
+          <StyledSearchFormButtonLabel>Search</StyledSearchFormButtonLabel>
+        </StyledSearchFormButton>
 
-          <StyledSearchFormInput
-            type="text"
-            autoComplete="off"
-            autoFocus
-            placeholder="Search images and photos"
-            value={searchImage}
-            onChange={this.handleChange}
-          />
-        </StyledSearchForm>
-      </StyledSearchbar>
-    );
-  }
-}
+        <StyledSearchFormInput
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+          value={searchImage}
+          onChange={handleChange}
+        />
+      </StyledSearchForm>
+    </StyledSearchbar>
+  );
+};
