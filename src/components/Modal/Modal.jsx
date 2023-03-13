3ec4fc3f561ba710f7311ebd24e3e0572f92bb23
TetianaKokoilo <108227374+TetianaKokoilo@users.noreply.github.com>
2023-03-14 01:18:38 +0200

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,6 @@
-/* eslint-disable no-unused-vars */
-import { useEffect } from 'react';
-// import { createPortal } from 'react-dom';
-import { StyledOverlay, StyledModal } from './Modal.styled';
-// import PropTypes from 'prop-types';
 
-// const modalRoot = document.querySelector('#modal-root');
+import { useEffect } from 'react';
+import { StyledOverlay, StyledModal } from './Modal.styled';
 
 export const Modal = ({largeImageURL, tags, onClose}) => {
   useEffect(() => {
@@ -32,40 +28,3 @@ export const Modal = ({largeImageURL, tags, onClose}) => {
     </StyledOverlay>
   );
 };
-// export class Modal extends Component {
-//   componentDidMount() {
-//     // console.log('Modal componentDidMount');
-//     window.addEventListener('keydown', this.hadleKeyDown);
-//   }
-
-//   componentWillUnmount() {
-//     // console.log('Modal componentWillUnmount');
-//     window.removeEventListener('keydown', this.hadleKeyDown);
-//   }
-
-//   hadleKeyDown = e => {
-//     if (e.code === 'Escape') {
-//       this.props.onClose();
-//     }
-//   };
-
-//   handleBackdropClick = e => {
-//     if (e.currentTarget === e.target) {
-//       this.props.onClose();
-//     }
-//   };
-
-//   render() {
-//     const children = this.props.children;
-//     return createPortal(
-//       <StyledOverlay onClick={this.handleBackdropClick}>
-//             <StyledModal>{children}</StyledModal>
-//       </StyledOverlay>,
-//       modalRoot
-//     );
-//   }
-// }
-
-// Modal.propTypes = {
-//   children: PropTypes.element.isRequired,
-// }
